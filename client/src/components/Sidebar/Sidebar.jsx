ff02f3daa10678d9a0d292788f32b474264a3a1d
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-08-31 17:44:21 -0400

    add main categories

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,12 @@
-
+import styles from "./Sidebar.module.css";
 export default function Sidebar() {
     return(
-        <>
-        </>
+        <div className={styles.sidebar}>
+            <ul>
+                <li>All Notes</li>
+                <li>Favorites</li>
+                <li>Trash</li>
+            </ul>
+        </div> 
     );
 }
