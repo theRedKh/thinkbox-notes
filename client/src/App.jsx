9be1676ce4dbe7bb0d0c5d918ed18a1f18592ed4
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-08-31 17:33:42 -0400

    initial for menus

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import NoteForm from './components/NoteForm/NoteForm';
 import Toolbar from './components/Toolbar/Toolbar';
-
+import NotesList from './components/NotesList/NotesList';
+import Sidebar from './components/Sidebar/Sidebar';
 function App() {
   
 
   return (
-    <>
+    <div className='app-container'>
+      <Sidebar/>
+      <NotesList/>
       <NoteForm/>
-    </>
+    </div>
   )
 }
 
---
+++ client/src/components/NotesList/NotesList.jsx
@@ -0,0 +1,7 @@
+
+export default function NotesList() {
+    return(
+        <>
+        </>
+    );
+}
