d35cca85be5c2fb9bfb22752b958c28da37006e6
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-08-31 17:49:03 -0400

    add elements for sidebar

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
 import styles from "./Sidebar.module.css";
+
 export default function Sidebar() {
+    const [folders, setFolders] = useState(["Work", "Personal", "Ideas"]);
+
+    const addFolder = () => {
+        const name = prompt("Enter folder name:");
+        if (name && !folders.includes(name)) {
+            setFolders([...folders, name]);
+        }
+    }
+
     return(
         <div className={styles.sidebar}>
             <ul>
@@ -7,6 +18,16 @@ export default function Sidebar() {
                 <li>Favorites</li>
                 <li>Trash</li>
             </ul>
+            {/* Folders section */}
+            <h3 className={styles.sectionTitle}>Folders</h3>
+            <ul className={styles.list}>
+                {folders.map((folder, index) => (
+                    <li key={index}>{folder}</li>
+                ))}
+            </ul>
+
+            {/* Add Folder btn */}
+            <button className={styles.addFolder} onClick={addFolder}> + Add Folder </button>
         </div> 
     );
 }
