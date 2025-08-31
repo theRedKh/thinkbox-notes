2a5758bbbd41a337ea21da0ee1d511a33f398a66
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-08-31 17:17:22 -0400

    toolbar optimization minor changes

--- client/src/components/NoteForm/NoteForm.jsx
+++ client/src/components/NoteForm/NoteForm.jsx
@@ -1,12 +1,11 @@
 import styles from "./NoteForm.module.css";
-
+import Toolbar from "../Toolbar/Toolbar";
 export default function NoteForm() {
     return (
         <div className={styles.container}>
-            <button className={styles.close}> x </button>
             <input placeholder="Title" className={styles.title}/>
-            <p className={styles.dateTime}></p>
             <textarea className={styles.noteArea} placeholder="What's on your mind..."></textarea>
+             <Toolbar/>
         </div>
     );
 }
--- client/src/components/Toolbar/Toolbar.jsx
+++ client/src/components/Toolbar/Toolbar.jsx
@@ -3,9 +3,12 @@ import styles from "./Toolbar.module.css";
 export default function Toolbar(){
     return(
         <div className={styles.container}>
-            <button className={styles.btn}></button>
-            <button className={styles.btn}></button>
-            <button className={styles.btn}></button>
+            <button className={styles.btn}><b>B</b></button>
+            <button className={styles.btn}><em>I</em></button>
+            <button className={styles.btn}><u>U</u></button>
+            <button className={styles.btn}>List Bullets</button>
+            <button className={styles.btn}>List Numerical</button>
+            <button className={styles.btn}>Font</button>
         </div>
     );
 }
