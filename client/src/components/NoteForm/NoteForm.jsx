3e8450c63abf3d80b12435bef1d5ef59a59871cd
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-08-31 17:26:28 -0400

    date time feature

--- client/src/components/NoteForm/NoteForm.jsx
+++ client/src/components/NoteForm/NoteForm.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
 import styles from "./NoteForm.module.css";
 import Toolbar from "../Toolbar/Toolbar";
+
 export default function NoteForm() {
+    const [dateTime, setDateTime] = useState("");
+
+    useEffect(() => {
+        const now= new Date();
+        const day = now.getDate();
+        const month = now.toLocaleString("en-US", { month: "long" });
+        const year = now.getFullYear();
+        const time = now.toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "2-digit",
+            hour12: true,
+        });
+
+        const formatted = `${day} ${month}, ${year} at ${time}`;
+        setDateTime(formatted);
+    }, []);
+
     return (
         <div className={styles.container}>
             <input placeholder="Title" className={styles.title}/>
+            <p className={styles.dateTime}>{dateTime}</p>
             <textarea className={styles.noteArea} placeholder="What's on your mind..."></textarea>
              <Toolbar/>
         </div>
