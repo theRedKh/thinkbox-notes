61da242436abde290eef2d6563904e2b560e942c
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-09-28 12:41:46 -0400

    toolbar update

--- client/src/components/Toolbar/Toolbar.jsx
+++ client/src/components/Toolbar/Toolbar.jsx
@@ -1,13 +1,14 @@
 import styles from "./Toolbar.module.css";
-
+import listB from "../../assets/bullet_list.svg";
+import listN from "../../assets/numbered_list.svg";
 export default function Toolbar({ onBold, onItalic, onUnderline, onBulletn, onNumbered, onFont, onSave }) {
     return (
         <div className={styles.container}>
             <button className={styles.btn} onClick={onBold}><b>B</b></button>
             <button className={styles.btn} onClick={onItalic}><em>I</em></button>
             <button className={styles.btn} onClick={onUnderline}><u>U</u></button>
-            <button className={styles.btn} onClick={onBulletn}>List Bullets</button>
-            <button className={styles.btn} onClick={onNumbered}>List Numerical</button>
+            <button className={styles.btn} onClick={onBulletn}><img src={listB}/></button>
+            <button className={styles.btn} onClick={onNumbered}><img src={listN}/></button>
             <button className={styles.btn} onClick={onFont}>Font</button>
             {/* Save */}
             <button 
