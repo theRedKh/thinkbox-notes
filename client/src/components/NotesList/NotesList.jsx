1bec550ee5356a10fa4d0e7b19248d8aa05ce26f
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-12-27 15:26:36 +0500

    note icons colors

--- client/src/components/NotesList/NotesList.jsx
+++ client/src/components/NotesList/NotesList.jsx
@@ -161,7 +161,7 @@ export default function NotesList({
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                       <path d="M8.00001 1.33333L10.06 5.50666L14.6667 6.18L11.3333 9.42666L12.12 14.0133L8.00001 11.8467L3.88001 14.0133L4.66668 9.42666L1.33334 6.18L5.94001 5.50666L8.00001 1.33333Z" 
-                      stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round"/>
+                      stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                   </span>
 
@@ -199,11 +199,11 @@ export default function NotesList({
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                       <path d="M10 10.8333C10.4603 10.8333 10.8334 10.4602 10.8334 10C10.8334 9.53976 10.4603 9.16666 10 9.16666C9.53978 9.16666 9.16669 9.53976 9.16669 10C9.16669 10.4602 9.53978 10.8333 10 10.8333Z" 
-                      stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round"/>
+                      stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round"/>
                       <path d="M10 4.99999C10.4603 4.99999 10.8334 4.6269 10.8334 4.16666C10.8334 3.70642 10.4603 3.33333 10 3.33333C9.53978 3.33333 9.16669 3.70642 9.16669 4.16666C9.16669 4.6269 9.53978 4.99999 10 4.99999Z" 
-                      stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round"/>
+                      stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round"/>
                       <path d="M10 16.6667C10.4603 16.6667 10.8334 16.2936 10.8334 15.8333C10.8334 15.3731 10.4603 15 10 15C9.53978 15 9.16669 15.3731 9.16669 15.8333C9.16669 16.2936 9.53978 16.6667 10 16.6667Z" 
-                      stroke="#1E1E1E" stroke-linecap="round" stroke-linejoin="round"/>
+                      stroke="#1E1E1E" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>
                   </span>
                   <span
