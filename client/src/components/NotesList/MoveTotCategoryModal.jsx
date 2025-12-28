49971455e6998616bb987c505c1e908380aa0362
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-12-28 16:31:29 +0500

    jsx for modal (minimal)

--- client/src/components/NotesList/MoveTotCategoryModal.jsx
+++ client/src/components/NotesList/MoveTotCategoryModal.jsx
@@ -1,1 +1,33 @@
 import styles from "./NoteslIst.module.css";
+
+export default function MoveToCategoryModal({
+    categories,
+    currentCategory,
+    onSelect,
+    onClose,
+}) {
+    return (
+        <div className={styles.modalOverlay} onClick={onClose}>
+            <div
+                className={styles.modal}
+                onClick={(e)=> e.stopPropagation()} //
+            >
+                <h4 className={styles.modalTitle}>Move to...</h4>
+
+                <ul className={styles.categoryList}>
+                    {categories.map((cat) => (
+                        <li
+                            key={cat}
+                            className={`${styles.categoryItem} ${
+                                cat === currentCategory ? styles.activeCategory : ""
+                            }`}
+                            onClick={() => onSelect(cat)}
+                        >
+                            {cat}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
