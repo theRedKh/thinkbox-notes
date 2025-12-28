bcd66e627866c50065f36642f0bd319226fe2efe
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2025-12-28 16:32:18 +0500

    doc

--- client/src/components/NotesList/MoveTotCategoryModal.jsx
+++ client/src/components/NotesList/MoveTotCategoryModal.jsx
@@ -10,12 +10,12 @@ export default function MoveToCategoryModal({
         <div className={styles.modalOverlay} onClick={onClose}>
             <div
                 className={styles.modal}
-                onClick={(e)=> e.stopPropagation()} //
+                onClick={(e)=> e.stopPropagation()} //prevents clicking inside to close modal issue
             >
                 <h4 className={styles.modalTitle}>Move to...</h4>
 
                 <ul className={styles.categoryList}>
-                    {categories.map((cat) => (
+                    {categories.map((cat) => ( //map categories from an imported list of categories to each list item -- dynamic modal
                         <li
                             key={cat}
                             className={`${styles.categoryItem} ${
